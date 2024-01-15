db6e19d78a8afba48ee74dc165e1ff7ecbd9982b
Daniel Hauge <Animcuil@gmail.com>
2024-01-15 13:42:24 +0100

    gg

--- GpuProgramming/hello-world/helloworld.c
+++ GpuProgramming/hello-world/helloworld.c
@@ -3,16 +3,19 @@
 #include <stdlib.h>
 int main(int argc, char *argv[]) {
 #pragma omp target teams parallel num_teams(4) thread_limit(64)
-	{
-  int numTeam = omp_get_team_num();
-  int numThread = omp_get_thread_num();
-  int global_tid = numTeam * 64 + numThread;
-  if (global_tid == 100) { int *a = (int*) 0x10000; *a = 0; }
-  printf("Hello world! "
-         "I’m thread %i out of 64 in team %i. "
-         "My global thread id "
-         "is %i out of 256\n",
-         numThread, numTeam, global_tid);
-	}
+  {
+    int numTeam = omp_get_team_num();
+    int numThread = omp_get_thread_num();
+    int global_tid = numTeam * 64 + numThread;
+    if (global_tid == 100) {
+      int *a = (int *)0x10000;
+      *a = 0;
+    }
+    printf("Hello world! "
+           "I’m thread %i out of 64 in team %i. "
+           "My global thread id "
+           "is %i out of 256\n",
+           numThread, numTeam, global_tid);
+  }
   return EXIT_SUCCESS;
 }
