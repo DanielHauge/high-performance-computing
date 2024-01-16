3d2e36c8d7098420fa97863ed3a689e8e11e7cc4
Daniel Hauge <Animcuil@gmail.com>
2024-01-16 14:32:34 +0100

    gg

--- GpuProgramming/mxv/main.c
+++ GpuProgramming/mxv/main.c
@@ -1,3 +1,4 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define ITER 500
@@ -21,25 +22,43 @@ double **dmalloc_2d(int m, int n) {
 
 // matrix vector
 void matrix_vector(double **matrix, double *vector, double *result) {
+  double t = omp_get_wtime();
+#pragma omp taget data map(to : matrix[0 : N / 2][0 : N / 2],                  \
+                               vector[0 : N / 2])                              \
+    map(from : result[0 : N / 2])
+  {
+    double t = omp_get_wtime();
+
 #pragma omp target teams distribute parallel for nowait num_teams(N / 2)       \
     thread_limit(1) device(0)                                                  \
     map(to : matrix[0 : N / 2][0 : N / 2], vector[0 : N / 2])                  \
     map(from : result[0 : N / 2])
-  for (int i = 0; i < N / 2; i++) {
-    for (int j = 0; j < N / 2; j++) {
-      result[i] += matrix[i][j] * vector[j];
+    for (int i = 0; i < N / 2; i++) {
+      for (int j = 0; j < N / 2; j++) {
+        result[i] += matrix[i][j] * vector[j];
+      }
     }
+    printf("1. w/o allo: %f\n", omp_get_wtime() - t);
   }
+  printf("1. w allo: %f\n", omp_get_wtime() - t);
+  double t2 = omp_get_wtime();
+#pragma omp target data map(to : matrix[N / 2 : N / 2][0 : N / 2],             \
+                                vector[0 : N / 2])
+  {
+    double t = omp_get_wtime();
 
 #pragma omp target teams distribute parallel for nowait num_teams(N / 2)       \
     thread_limit(1) device(1)                                                  \
     map(to : matrix[N / 2 : N / 2][N / 2 : N / 2], vector[N / 2 : N / 2])      \
     map(from : result[N / 2 : N / 2])
-  for (int i = N / 2; i < N; i++) {
-    for (int j = N / 2; j < N; j++) {
-      result[i] += matrix[i][j] * vector[j];
+    for (int i = N / 2; i < N; i++) {
+      for (int j = N / 2; j < N; j++) {
+        result[i] += matrix[i][j] * vector[j];
+      }
     }
+    printf("2. w/o allo: %f\n", omp_get_wtime() - t);
   }
+  printf("2. w allo: %f\n", omp_get_wtime() - t2);
 
 #pragma omp taskwait
 }
@@ -60,7 +79,7 @@ int main(int argc, char *argv[]) {
   }
 
   for (int i = 0; i < ITER; i++) {
-    matrix_vector(matrix, vector, result, 0, N);
+    matrix_vector(matrix, vector, result);
   }
 
   // Print result
