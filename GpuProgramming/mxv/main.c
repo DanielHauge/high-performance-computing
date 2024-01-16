fe59921dda64b69d0cec658e053406f39e463987
Daniel Hauge <Animcuil@gmail.com>
2024-01-16 12:47:06 +0100

    format:gg

--- GpuProgramming/mxv/main.c
+++ GpuProgramming/mxv/main.c
@@ -2,50 +2,62 @@
 #include <stdlib.h>
 #define ITER 500
 #define N 5000
-#define NN 5000*5000
-double** dmalloc_2d(int m, int n) { if (m <= 0 || n <= 0) return NULL; double **A = (double**)malloc(m * sizeof(double *)); if (A == NULL) return NULL; A[0] = (double *)malloc(m*n*sizeof(double)); if (A[0] == NULL) { free(A); return NULL; } for (int i = 1; i < m; i++) A[i] = A[0] + i * n; return A; }
-
+#define NN 5000 * 5000
+double **dmalloc_2d(int m, int n) {
+  if (m <= 0 || n <= 0)
+    return NULL;
+  double **A = (double **)malloc(m * sizeof(double *));
+  if (A == NULL)
+    return NULL;
+  A[0] = (double *)malloc(m * n * sizeof(double));
+  if (A[0] == NULL) {
+    free(A);
+    return NULL;
+  }
+  for (int i = 1; i < m; i++)
+    A[i] = A[0] + i * n;
+  return A;
+}
 
 // matrix vector
 void matrix_vector(double **matrix, double *vector, double *result, int n) {
-#pragma omp target data map(to:matrix[:NN],vector[:N]) map(tofrom:result[:N])
+#pragma omp target data map(to : matrix[ : NN], vector[ : N])                  \
+    map(tofrom : result[ : N])
   {
-#pragma omp target teams loop map(tofrom:matrix[:N*N],vector[:N]) map(tofrom:result[:N])
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-       result[i]+= matrix[i][j] * vector[j];
+#pragma omp target teams loop map(tofrom : matrix[ : N * N], vector[ : N])     \
+    map(tofrom : result[ : N])
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        result[i] += matrix[i][j] * vector[j];
+      }
     }
-  }
   }
 }
 
 int main(int argc, char *argv[]) {
   // Allocate matrix and vector
-  double **matrix = dmalloc_2d(N,N);
+  double **matrix = dmalloc_2d(N, N);
   double *vector = (double *)malloc(N * sizeof(double));
   double *result = (double *)malloc(N * sizeof(double));
-
 
   // Initialize matrix and vector
   for (int i = 0; i < N; i++) {
     vector[i] = i;
     result[i] = 0;
     for (int j = 0; j < N; j++) {
-	    matrix[i][j] = (double)i+j;
+      matrix[i][j] = (double)i + j;
     }
   }
 
-  for (int i = 0; i < ITER; i++){
-  	matrix_vector(matrix, vector, result, N);
+  for (int i = 0; i < ITER; i++) {
+    matrix_vector(matrix, vector, result, N);
   }
-
-
 
   // Print result
   double d = 0;
-	for (int i = 0; i < N; i++){
-	d += result[i];	
-	}
-	printf("%f", d);
+  for (int i = 0; i < N; i++) {
+    d += result[i];
+  }
+  printf("%f", d);
   return EXIT_SUCCESS;
 }
